b95a876cfe12b1572194e654b10efd1d0e5f1b70
Konrad Niemiec <konradjniemiec@gmail.com>
2022-08-15 11:36:58 -0700

    compile skeleton

---
+++ pkg/feature/feature.go
@@ -0,0 +1,40 @@
+// Copyright 2022 Lekko Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This package governs the specifics of a feature, like what individual
+// files make up a feature.
+package feature
+
+import (
+	"github.com/lekkodev/cli/pkg/metadata"
+)
+
+// FeatureFile is a parsed feature from an on desk representation.
+type FeatureFile struct {
+	Name string
+	// Filename of the featureName.build.star file.
+	BuilderFileName string
+	// Filename of the featureName.validate.star file.
+	// This is optional.
+	ValidatorFileName string
+	// Filename of an featureName.proto file.
+	// This is optional.
+	ProtoFileName string
+}
+
+// This groups feature files in a way that is
+// governed by the namespace metadata.
+func GroupFeatureFiles(pathToNamespace string, nsMD *metadata.NamespaceConfigRepoMetadata) ([]FeatureFile, error) {
+	return nil, nil
+}
