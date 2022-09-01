32907c4d0584322f3ca92ef50275bd8557678202
Shubhit Singh <3323832+shubhitms@users.noreply.github.com>
2022-09-01 11:03:09 -0700

    lekko review & merge (#35)
    * lekko review & merge
    
    * rm print

--- go.mod
+++ go.mod
@@ -4,12 +4,31 @@ go 1.18
 
 require (
 	github.com/bufbuild/connect-go v0.3.0
+	github.com/go-git/go-git/v5 v5.4.2
 	github.com/lekkodev/rules v1.3.0
 	github.com/spf13/cobra v1.5.0
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
 	google.golang.org/protobuf v1.28.0
 	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/Microsoft/go-winio v0.4.16 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
 
 require (
@@ -17,12 +36,10 @@ require (
 	github.com/blang/semver v3.5.1+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stripe/skycfg v0.1.1-0.20220125220303-4571141c5da1
 	go.starlark.net v0.0.0-20220815112429-e1b9ebd162dd
-	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
---
+++ pkg/gh/git.go
@@ -0,0 +1,163 @@
+// Copyright 2022 Lekko Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gh
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/pkg/errors"
+)
+
+const (
+	mainBranchName = "main"
+)
+
+// Abstraction around git and github operations associated with the lekko configuration repo.
+type ConfigRepo struct {
+	repo *git.Repository
+	wt   *git.Worktree
+}
+
+func New(path string) (*ConfigRepo, error) {
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open git repo")
+	}
+	wt, err := repo.Worktree()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get work tree")
+	}
+	return &ConfigRepo{
+		repo: repo,
+		wt:   wt,
+	}, nil
+}
+
+func (cr *ConfigRepo) Review() error {
+	isMain, err := cr.isMain()
+	if err != nil {
+		return errors.Wrap(err, "is main")
+	}
+	if !isMain {
+		return errors.New("can only start reviews off the main branch")
+	}
+
+	clean, err := cr.wdClean()
+	if err != nil {
+		return errors.Wrap(err, "wd clean")
+	}
+	if clean {
+		return errors.New("current working directory is clean, nothing to review")
+	}
+
+	branchName, err := cr.checkoutBranch()
+	if err != nil {
+		return errors.Wrap(err, "checkout new branch")
+	}
+
+	if err := cr.wt.AddGlob("."); err != nil {
+		return errors.Wrap(err, "add glob")
+	}
+	log.Printf("checked out new branch %s\n", branchName)
+
+	hash, err := cr.wt.Commit("new config changes", &git.CommitOptions{
+		All: true,
+	})
+	if err != nil {
+		return errors.Wrap(err, "commit")
+	}
+	log.Printf("committed new hash %s\n", hash)
+	if err := cr.createPR(branchName); err != nil {
+		return errors.Wrap(err, "create pr")
+	}
+	return nil
+}
+
+func (cr *ConfigRepo) Merge() error {
+	cmd := exec.Command("gh", "pr", "merge", "-sd")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		return errors.Wrap(err, "gh pr merge")
+	}
+	return nil
+}
+
+func (cr *ConfigRepo) isMain() (bool, error) {
+	h, err := cr.repo.Head()
+	if err != nil {
+		return false, errors.Wrap(err, "head")
+	}
+	return h.Name().IsBranch() && h.Name().Short() == mainBranchName, nil
+}
+
+func (cr *ConfigRepo) wdClean() (bool, error) {
+	st, err := cr.wt.Status()
+	if err != nil {
+		return false, errors.Wrap(err, "status")
+	}
+	return st.IsClean(), nil
+}
+
+func (cr *ConfigRepo) genBranchName() (string, error) {
+	cfg, err := config.LoadConfig(config.GlobalScope)
+	if err != nil {
+		return "", errors.Wrap(err, "load global config")
+	}
+	user := strings.ToLower(strings.Split(cfg.User.Name, " ")[0])
+	if user == "" {
+		user = "anon"
+	}
+	return fmt.Sprintf("%s-review-%d", user, time.Now().Nanosecond()), nil
+}
+
+func (cr *ConfigRepo) checkoutBranch() (string, error) {
+	branchName, err := cr.genBranchName()
+	if err != nil {
+		return "", errors.Wrap(err, "gen branch name")
+	}
+	if err := cr.wt.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.NewBranchReferenceName(branchName),
+		Create: true,
+		Keep:   true,
+	}); err != nil {
+		return "", errors.Wrap(err, "checkout")
+	}
+	return branchName, nil
+}
+
+func (cr *ConfigRepo) createPR(branchName string) error {
+	cmd := exec.Command("git", "push", "--set-upstream", "origin", branchName)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		return errors.Wrap(err, "git push set upstream")
+	}
+	cmd = exec.Command("gh", "pr", "create", "-B", mainBranchName, "-H", branchName, "-f")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		return errors.Wrap(err, "gh pr create")
+	}
+	return nil
+}
