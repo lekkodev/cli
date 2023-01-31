33af5c60548ae93337bbfc9ea6ea03dd0b96f866
Shubhit Singh <shubhit@lekko.dev>
2023-01-30 16:04:12 -0800

    oauth

--- cmd/lekko/main.go
+++ cmd/lekko/main.go
@@ -26,7 +26,7 @@ import (
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/go-git/go-git/v5"
-	"github.com/lekkodev/cli/auth"
+	"github.com/lekkodev/cli/oauth"
 	"github.com/lekkodev/cli/pkg/feature"
 	"github.com/lekkodev/cli/pkg/gh"
 	"github.com/lekkodev/cli/pkg/k8s"
@@ -310,7 +310,7 @@ var loginCmd = &cobra.Command{
 	Use:   "login",
 	Short: "authenticate with github",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		auth := auth.NewAuthFS()
+		auth := oauth.NewAuthFS()
 		defer auth.Close()
 		return auth.Login(cmd.Context())
 	},
@@ -320,7 +320,7 @@ var logoutCmd = &cobra.Command{
 	Use:   "logout",
 	Short: "log out of github",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		auth := auth.NewAuthFS()
+		auth := oauth.NewAuthFS()
 		defer auth.Close()
 		return auth.Logout(cmd.Context())
 	},
@@ -330,7 +330,7 @@ var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "display lekko authentication status",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		auth := auth.NewAuthFS()
+		auth := oauth.NewAuthFS()
 		auth.Status(cmd.Context())
 		return nil
 	},
