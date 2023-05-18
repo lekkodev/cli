539b2ce7822a4bf012ac06f326aa0e5cf0278278
Shubhit Singh <shubhit@lekko.dev>
2023-05-18 15:38:26 -0700

    dry run proto format

--- pkg/repo/feature.go
+++ pkg/repo/feature.go
@@ -523,7 +523,7 @@ func (r *repository) FormatFeature(ctx context.Context, ff *feature.FeatureFile,
 		r,
 		// dry-run for JSON features, because static formatting
 		// is unstable due to non-determinism of ordering keys.
-		fType == feature.FeatureTypeJSON,
+		fType == feature.FeatureTypeJSON || fType == feature.FeatureTypeProto,
 		registry,
 	)
 	// try static formatting
