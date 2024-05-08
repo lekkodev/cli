b5ba2c3531e90708d1900d29ba40f530ad4c354b
Sergey Passichenko <sergey@lekko.com>
2024-05-08 12:38:06 -0700

    fix: create lekko dir before trying to list namespaces

--- pkg/gen/gen.go
+++ pkg/gen/gen.go
@@ -31,6 +31,10 @@ func GenNative(ctx context.Context, nativeLang native.NativeLang, lekkoPath, rep
 	defer err2.Handle(&err)
 
 	absLekkoPath := filepath.Join(nativeRepoPath, lekkoPath)
+	err = os.MkdirAll(absLekkoPath, 0770)
+	if err != nil {
+		return errors.Wrap(err, "create output dir")
+	}
 
 	var namespaces []string
 	if len(namespace) == 0 {
@@ -43,10 +47,6 @@ func GenNative(ctx context.Context, nativeLang native.NativeLang, lekkoPath, rep
 	case native.TS:
 		if initMode {
 			return errors.New("init mode not supported for TS")
-		}
-		err := os.MkdirAll(absLekkoPath, 0770)
-		if err != nil {
-			return errors.Wrap(err, "create output dir")
 		}
 		for _, ns := range namespaces {
 			outFilename := filepath.Join(absLekkoPath, ns+nativeLang.Ext())
