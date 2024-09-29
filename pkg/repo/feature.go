7faf326697ae38fb3723b50c42e4e13acd394df2
David Kang <david@lekko.com>
2024-09-29 11:37:53 -0700

    Add copying and building buf image

--- pkg/repo/feature.go
+++ pkg/repo/feature.go
@@ -681,10 +681,9 @@ func (r *repository) BuildDynamicTypeRegistry(ctx context.Context, protoDirPath 
 // because we need to first ensure that buf cmd line can be executed in the
 // ephemeral env.
 func (r *repository) ReBuildDynamicTypeRegistry(ctx context.Context, protoDirPath string, useExternalTypes bool) (*protoregistry.Types, error) {
-	// TODO: We do need a way of regenerating the buf image in ephemeral repos. Disabling this check for now.
-	// if !r.bufEnabled {
-	// 	return nil, errors.New("buf cmd line not enabled")
-	// }
+	if !r.bufEnabled {
+		return nil, errors.New("buf cmd line not enabled")
+	}
 	sTypes, err := prototypes.ReBuildDynamicTypeRegistry(ctx, protoDirPath, useExternalTypes, r)
 	if err != nil {
 		return nil, err
