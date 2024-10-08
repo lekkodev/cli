8f1fb879d0785537e38214fb57a8cde0fdf8cc81
David Kang <david@lekko.com>
2024-10-08 14:24:29 -0700

    Add custom sync error types and parse TS sync errors

--- pkg/sync/ts.go
+++ pkg/sync/ts.go
@@ -18,6 +18,8 @@ import (
 	"context"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/lekkodev/cli/pkg/gen"
@@ -27,6 +29,10 @@ import (
 
 	featurev1beta1 "buf.build/gen/go/lekkodev/cli/protocolbuffers/go/lekko/feature/v1beta1"
 )
+
+const syncTSExec = "lekko-sync"
+
+var NoSyncTSError = errors.New("missing npx lekko-sync")
 
 // TODO: consider consolidating bisync methods
 func BisyncTS(ctx context.Context, lekkoPath, repoPath string) error {
@@ -49,28 +55,49 @@ func BisyncTS(ctx context.Context, lekkoPath, repoPath string) error {
 
 func SyncTS(lekkoPath string) (*featurev1beta1.RepositoryContents, error) {
 	// TODO: If command not found, give helpful error message prompting user to update dependencies
-	tsSyncCmd := exec.Command("npx", "lekko-sync", "--lekko-dir", lekkoPath)
+	tsSyncCmd := exec.Command("npx", syncTSExec, "--lekko-dir", lekkoPath)
 	output, err := tsSyncCmd.CombinedOutput()
 	if err != nil {
-		return nil, errors.Wrapf(err, "sync ts: %s", output)
+		return nil, checkSyncTSError(output)
 	}
 	repoContents, err := repo.DecodeRepositoryContents(output)
 	if err != nil {
-		return nil, errors.Wrap(err, "decode sync ts output")
+		return nil, NewSyncError(errors.Wrap(err, "decode"))
 	}
 	return repoContents, nil
 }
 
 func SyncTSFiles(lekkoFiles ...string) (*featurev1beta1.RepositoryContents, error) {
 	commafied := strings.Join(lekkoFiles, ",")
-	tsSyncCmd := exec.Command("npx", "lekko-sync", "--lekko-files", commafied)
+	tsSyncCmd := exec.Command("npx", syncTSExec, "--lekko-files", commafied)
 	output, err := tsSyncCmd.CombinedOutput()
 	if err != nil {
-		return nil, errors.Wrapf(err, "sync ts: %s", output)
+		return nil, checkSyncTSError(output)
 	}
 	repoContents, err := repo.DecodeRepositoryContents(output)
 	if err != nil {
-		return nil, errors.Wrap(err, "decode sync ts output")
+		return nil, NewSyncError(errors.Wrap(err, "decode"))
 	}
 	return repoContents, nil
+}
+
+func checkSyncTSError(output []byte) error {
+	o := string(output)
+	if strings.Contains(o, "404") && strings.Contains(o, syncTSExec) {
+		return NewSyncError(NoSyncTSError)
+	}
+	r := regexp.MustCompile("LekkoParseError: (.*):(\\d+):(\\d+) - (.*)")
+	matches := r.FindStringSubmatch(o)
+	if matches == nil {
+		return NewSyncError(errors.New(o))
+	}
+	line, err := strconv.Atoi(matches[2])
+	if err != nil {
+		return NewSyncError(errors.New(o))
+	}
+	col, err := strconv.Atoi(matches[3])
+	if err != nil {
+		return NewSyncError(errors.New(o))
+	}
+	return NewSyncPosError(errors.New(matches[4]), matches[1], line, col)
 }
